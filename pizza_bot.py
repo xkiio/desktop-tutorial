e1ef28a22751377220c38aff40146d07b27dedab
xkiio <99291539+xkiio@users.noreply.github.com>
2022-02-14 10:57:06 +1300

    Added comments for each section.
    Comments for each section

--- pizza_bot.py
+++ pizza_bot.py
@@ -1,1 +1,69 @@
 #Pizza bot programme
+
+
+#Welcome message with random name
+
+
+
+
+
+#Menu for pickup or delivery
+
+
+
+
+
+
+# Pick up information - name and phone number
+
+
+
+
+
+
+#Delivery information - name address and phone
+
+
+
+
+
+#Choose total number of pizzas - max 5
+
+
+
+
+
+
+#Pizza menu
+
+
+
+
+
+
+#Pizza order - from menu - print each pizza ordered with cast
+
+
+
+
+
+# Print order out - including if order is del or pick up and names and price of each pizza - total cost including any delivery charge
+
+
+
+
+#Ability to cancel or proceed with order
+
+
+
+
+
+#Option for new order or exit
+
+
+
+
+
+
+
+#Main function
