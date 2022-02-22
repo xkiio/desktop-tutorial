e06a94d0036fef188b3c784c3d34be9627e7dd38
xkiio <99291539+xkiio@users.noreply.github.com>
2022-02-23 09:41:32 +1300

    Created version 1 of pickup function
    Basic instructions and input for name and phone number.

---
+++ Components/pickup-v1.py
@@ -0,0 +1,13 @@
+#Bug - accepts blank input
+
+print("Please enter the pickup information.")
+
+#customer name
+name = input("Please enter your name: ")
+
+#customer phone number
+phone = input("Please enter your phone number: ")
+
+print("")
+print (name)
+print (phone)
