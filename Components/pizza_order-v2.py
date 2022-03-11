7c305dbca74bb168be405454dd41b7968cb112c5
xkiio <99291539+xkiio@users.noreply.github.com>
2022-03-11 15:12:24 +1300

    added validation to #
    added validation to number of pizza entry to restrict number to between 1 and  and only accept valid integer

---
+++ Components/pizza_order-v2.py
@@ -0,0 +1,47 @@
+#list of pizza names
+pizza_names = ['Margherita','Pepperoni','Hawaiian','Cheese','Italian','Veggie','Vegan','Chicken Deluxe',
+                'Mega Meat Lovers','Seafood Deluxe','Apricot Chicken Deluxe','BBQ Chicken Deluxe']
+#lists of pizza prices
+pizza_prices = [8.50, 8.50, 8.50, 8.50, 8.50, 8.50, 8.50, 13.50, 13.50, 13.50, 13.50, 13.50]
+
+#list to store ordered pizzas
+order_list = []
+#list to store pizzas prices
+order_cost = []
+
+#list to store order cost
+
+def menu():
+    number_pizza = 12
+
+    for count in range(number_pizza) :
+        print("{} {} ${:.2f}" .format(count+1, pizza_names[count],pizza_prices[count]))
+
+menu()
+
+# ask for total number of pizzas for order
+num_pizzas = 0
+
+
+while True:
+    try:
+        num_pizzas = int(input("How many pizzas do you want to order? "))
+        if num_pizzas >= 1 and num_pizzas <= 5:
+            break
+        else:
+            print("Your order must be between 1 and 5")
+    except ValueError:
+        print ("That is not a valid number.")
+        print ("Please enter 1 or 5")
+print(num_pizzas)
+
+#Choose pizza from menu
+print ("Please choose your pizzas by entering the number from the menu: ")
+for item in range(num_pizzas):
+    while num_pizzas > 0:
+        pizza_ordered = int(input())
+        pizza_ordered = pizza_ordered -1
+        order_list.append(pizza_names[pizza_ordered])
+        order_cost.append(pizza_prices[pizza_ordered])
+        print("{} ${:.2f}" .format(pizza_names[pizza_ordered],pizza_prices[pizza_ordered]))
+        num_pizzas = num_pizzas-1
