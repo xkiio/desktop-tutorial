2fa4e58278903acfb7982316249d950692526f1b
xkiio <99291539+xkiio@users.noreply.github.com>
2022-03-23 09:59:18 +1300

    customer details
    added code to print customer details

--- Components/order_print-v1.py
+++ Components/order_print-v1.py
@@ -1,9 +1,13 @@
-#lists of pizza prices
-pizza_prices = [8.50, 8.50, 8.50, 8.50, 8.50, 8.50, 8.50, 13.50, 13.50, 13.50, 13.50, 13.50]
 #list to store ordered pizzas
 order_list = ['Margherita','Pepperoni','Hawaiian','Cheese']
 #list to store pizzas prices
 order_cost = [8.50, 8.50, 8.50, 8.50,]
+
+cost_details = {"name": "Mark","phone": "1234567876","house": "45","street": "harry","suburb": "howick"}
+
+
+
+print("\n Customer Name: {} Customer Phone: \n{} Customer House Number: \n{} Customer Street Name: \n{} Customer Suburb : \n{}" .format(cost_details['name'],cost_details['phone'], cost_details['house'],cost_details['street'], cost_details['suburb']))
 
 count = 0
 for item in order_list:
